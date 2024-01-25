9e915f9f2ec4aebe67fef73dad1e92102823df7f
JosiahFu <josiahfu@gmail.com>
2024-01-24 20:33:11 -0800

    Init project with Vite

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+export default {
+    tabWidth: 4,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
+    singleAttributePerLine: false,
+    plugins: ['prettier-plugin-tailwindcss'],
+};
